fc8d54a79d25e9708d6efa39a473f03c28728c61
Magnus Müller <mamagnus00@gmail.com>
2021-12-08 18:06:13 -0600

    countdown more beautiful

--- lib/pages/countdown.dart
+++ lib/pages/countdown.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
-
 
 class CountdownPage extends StatefulWidget {
   CountdownPage({Key? key, this.title, this.time}) : super(key: key);
@@ -15,19 +13,26 @@ class CountdownPage extends StatefulWidget {
 class _CountdownPageState extends State<CountdownPage> {
   CountDownController _controller = CountDownController();
   late int _duration;
+  bool started = false;
+  bool first = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    if ( widget.time != null){
-      _duration =  widget.time!.hour * 60 * 60 + widget.time!.minute * 60 + widget.time!.second ;
-
-    } else{
+    if (widget.time != null) {
+      _duration = widget.time!.hour * 60 * 60 +
+          widget.time!.minute * 60 +
+          widget.time!.second;
+    } else {
       _duration = 30 * 60;
     }
-    print("my time:" + _duration.toString() + "current widget time " + widget.time.toString());
+    print("my time:" +
+        _duration.toString() +
+        "current widget time " +
+        widget.time.toString());
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,119 +41,124 @@ class _CountdownPageState extends State<CountdownPage> {
       ),
       body: Center(
           child: CircularCountDownTimer(
-            // Countdown duration in Seconds.
-            duration: _duration,
+        // Countdown duration in Seconds.
+        duration: _duration,
 
-            // Countdown initial elapsed Duration in Seconds.
-            initialDuration: 0,
+        // Countdown initial elapsed Duration in Seconds.
+        initialDuration: 0,
 
-            // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
-            controller: _controller,
+        // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
+        controller: _controller,
 
-            // Width of the Countdown Widget.
-            width: MediaQuery
-                .of(context)
-                .size
-                .width / 2,
+        // Width of the Countdown Widget.
+        width: MediaQuery.of(context).size.width / 2,
 
-            // Height of the Countdown Widget.
-            height: MediaQuery
-                .of(context)
-                .size
-                .height / 2,
+        // Height of the Countdown Widget.
+        height: MediaQuery.of(context).size.height / 2,
 
-            // Ring Color for Countdown Widget.
-            ringColor: Colors.grey[300]!,
+        // Ring Color for Countdown Widget.
+        ringColor: Colors.grey[300]!,
 
-            // Ring Gradient for Countdown Widget.
-            ringGradient: null,
+        // Ring Gradient for Countdown Widget.
+        ringGradient: null,
 
-            // Filling Color for Countdown Widget.
-            fillColor: Colors.green[100]!,
+        // Filling Color for Countdown Widget.
+        fillColor: Colors.green[100]!,
 
-            // Filling Gradient for Countdown Widget.
-            fillGradient: null,
+        // Filling Gradient for Countdown Widget.
+        fillGradient: null,
 
-            // Background Color for Countdown Widget.
-            backgroundColor: Colors.green[500],
+        // Background Color for Countdown Widget.
+        backgroundColor: Colors.green[500],
 
-            // Background Gradient for Countdown Widget.
-            backgroundGradient: null,
+        // Background Gradient for Countdown Widget.
+        backgroundGradient: null,
 
-            // Border Thickness of the Countdown Ring.
-            strokeWidth: 20.0,
+        // Border Thickness of the Countdown Ring.
+        strokeWidth: 20.0,
 
-            // Begin and end contours with a flat edge and no extension.
-            strokeCap: StrokeCap.round,
+        // Begin and end contours with a flat edge and no extension.
+        strokeCap: StrokeCap.round,
 
-            // Text Style for Countdown Text.
-            textStyle: TextStyle(
-                fontSize: 33.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold),
+        // Text Style for Countdown Text.
+        textStyle: TextStyle(
+            fontSize: 33.0, color: Colors.white, fontWeight: FontWeight.bold),
 
-            // Format for the Countdown Text.
-            textFormat: CountdownTextFormat.MM_SS,
+        // Format for the Countdown Text.
+        textFormat: CountdownTextFormat.MM_SS,
 
-            // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
-            isReverse: true,
+        // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+        isReverse: true,
 
-            // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
-            isReverseAnimation: true,
+        // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+        isReverseAnimation: true,
 
-            // Handles visibility of the Countdown Text.
-            isTimerTextShown: true,
+        // Handles visibility of the Countdown Text.
+        isTimerTextShown: true,
 
-            // Handles the timer start.
-            autoStart: false,
+        // Handles the timer start.
+        autoStart: false,
 
-            // This Callback will execute when the Countdown Starts.
-            onStart: () {
-              // Here, do whatever you want
-              print('Countdown Started');
-            },
+        // This Callback will execute when the Countdown Starts.
+        onStart: () {
+          // Here, do whatever you want
+          print('Countdown Started');
+        },
 
-            // This Callback will execute when the Countdown Ends.
-            onComplete: () {
-              // Here, do whatever you want
-              print('Countdown Ended');
-            },
-          )),
+        // This Callback will execute when the Countdown Ends.
+        onComplete: () {
+          // Here, do whatever you want
+          print('Countdown Ended');
+        },
+      )),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            width: 30,
-          ),
-          _button(title: "Start", onPressed: () => _controller.start()),
-          SizedBox(
-            width: 10,
-          ),
-          _button(title: "Pause", onPressed: () => _controller.pause()),
-          SizedBox(
-            width: 10,
-          ),
-          _button(title: "Resume", onPressed: () => _controller.resume()),
-          SizedBox(
-            width: 10,
-          ),
-          _button(
-              title: "Restart",
-              onPressed: () => _controller.restart(duration: _duration))
+          (() {
+            // your code here
+            if (!started) {
+              //first time -> start
+              if (first) {
+                return _button(
+                    icon: Icon(Icons.play_circle_fill),
+                    title: "Start",
+                    onPressed: () {
+                      started = !started;
+                      first = !first;
+                      _controller.start();
+                    });
+              } else {
+                //next -> rsume
+                return _button(
+                    icon: Icon(Icons.play_circle_fill),
+                    title: "Resume",
+                    onPressed: () {
+                      started = !started;
+                      _controller.resume();
+                    });
+              }
+            } else {
+              return _button(
+                  icon: Icon(Icons.pause_circle_filled),
+                  title: "Pause",
+                  onPressed: () {
+                    started = !started;
+                    _controller.pause();
+                  });
+            }
+          }()),
         ],
       ),
     );
   }
 
-  _button({required String title, VoidCallback? onPressed}) {
+  _button(
+      {required String title, VoidCallback? onPressed, required Icon icon}) {
     return Expanded(
-        child: RaisedButton(
-          child: Text(
-            title,
-            style: TextStyle(color: Colors.white),
-          ),
-          onPressed: onPressed,
-          color: Colors.green,
-        ));
+        child: IconButton(
+      icon: icon,
+      onPressed: onPressed,
+      color: Colors.green,
+    ));
   }
 }
